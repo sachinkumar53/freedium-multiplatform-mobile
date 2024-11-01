dc48ffa8d9e28cb7b28c891f066e65806610c61f
Sachin <kr771.sachin@gmail.com>
2024-11-01 09:18:25 +0530

    Initial commit

---
+++ composeApp/src/commonMain/kotlin/com/sachin/freedium/Greeting.kt
@@ -0,0 +1,9 @@
+package com.sachin.freedium
+
+class Greeting {
+    private val platform = getPlatform()
+
+    fun greet(): String {
+        return "Hello, ${platform.name}!"
+    }
+}
---
+++ composeApp/src/iosMain/kotlin/com/sachin/freedium/MainViewController.kt
@@ -0,0 +1,5 @@
+package com.sachin.freedium
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App() }
